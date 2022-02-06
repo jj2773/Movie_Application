38473d7e009570f79b86d15c1f307b0f44960ba4
attiya waqqas <attiyawaqqas@gmail.com>
2022-02-05 20:16:32 -0500

    update

--- js/charts.js
+++ js/charts.js
@@ -1,40 +1,22 @@
 function init() {
     // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset");
+    var selector = d3.select("#data");
   
     // Use the list of sample names to populate the select options
     d3.json("js/data.json").then((data) => {
       var movies = data.movieId;
   
-      movies.forEach((movieId) => {
+      movies.forEach((data) => {
         selector
           .append("option")
-          .text(movieId)
-          .property("value", title_x);
-          
+          .text(data)
+          .property("value", data);
+        
+      })
       });
-      
-  
-      // Use the first sample from the list to build the initial plots
-      var firstMovie = movies[0];
-      buildCharts(firstMovie);
-      buildMetadata(firstMovie);
-    });
-  }
-  
-  // Initialize the dashboard
-  init();
-  
-  function optionChanged(newMovie) {
-    // Fetch new data each time a new sample is selected
-    buildMetadata(newMovie);
-    buildCharts(newMovie);
-    
-  }
   // Demographics Panel 
   function buildMetadata(movieID) {
-    d3.json("js/data.json").then((data) => {
-      var metadata = data.metadata;
+    d3.json("js/data.json").then((data);
       // Filter the data for the object with the desired sample number
       var resultArray = metadata.filter(movieObj => movieObj.id == movieID);
       var result = resultArray[0];
@@ -51,28 +33,7 @@ function init() {
         panel.append("h6").text(`${key}: ${value}`);
         {console.log(key + ': ' + value);}
         
-      });
-  
+      })
     });
-  }
-  
-  // 1. Create the buildCharts function.
-  function buildCharts(sample) {
-    // 2. Use d3.json to load and retrieve the samples.json file 
-    d3.json("js/data.json").then((data) => {
-      // 3. Create a variable that holds the samples array. 
-     var samples = data.samples;
-  
-      // 4. Create a variable that filters the samples for the object with the desired sample number.
-      var filterArray = samples.filter(sampleObject => sampleObject.id == sample);
-      //  5. Create a variable that holds the first sample in the array.
-      var result = filterArray[0];
-  
-    });
-  }
-  
-  
-  
-  
-    
-  
+}
+}
