f3134aabb387c9dc53091c685c7b478a8a03860b
attiya waqqas <attiyawaqqas@gmail.com>
2022-02-13 15:13:04 -0500

    Database Pull

--- js/charts.js
+++ js/charts.js
@@ -1,23 +1,29 @@
+var request = new XMLHttpRequest();
+    request.open("GET", "./js/data.json", false);
+    request.send(null)
+    var data = JSON.parse(request.responseText);
+
 function init() {
     // Grab a reference to the dropdown select element
     var selector = d3.select("#selDataset");
   
     // Use the list of sample names to populate the select options
-    d3.json("./js/data.json").then((data) => {
-      var sampleNames = data.names;
-      var clusterID=obj.Cluster;
-  
-      clusterID.forEach((val) => {
-        selector
-          .append("option")
-          .text(val)
-          .property("value", val);
-          
-      });
-      
+    d3.json("js/data.json").then((data) => {
+    var movieID=movieID
+        data.forEach((movieID) => {   
+            selector
+            .append("option")
+            .text(movieID) 
+        Object.values(movieID).forEach((val) => {
+            selector
+            .property("value", movieID);
+              });
+            
+            
+        });
   
       // Use the first sample from the list to build the initial plots
-      var firstCluster = obj.Cluster[0];
+      var firstCluster = cluster[0];
       buildCharts(firstCluster);
       buildMetadata(firstCluster);
     });
@@ -33,11 +39,11 @@ function init() {
     
   }
   // Demographics Panel 
-  function buildMetadata(clusterID) {
-    d3.json("js/data.json").then((clusterID) => {
-      var metadata = obj.title_x;
+  function buildMetadata(sample) {
+    d3.json("js/data.json").then((data) => {
+      var metadata = data.metadata;
       // Filter the data for the object with the desired sample number
-      var resultArray = obj.title_x.filter(sampleObj => sampleObj.id == obj.movieID);
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
       var result = resultArray[0];
       // Use d3 to select the panel with id of `#sample-metadata`
       var panel = d3.select("#sample-metadata");
