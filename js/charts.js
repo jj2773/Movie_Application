8b9fbf7e5e3df40999641264d38ba230d865d558
attiya waqqas <attiyawaqqas@gmail.com>
2022-02-13 21:38:36 -0500

    commit on 'main'

--- js/charts.js
+++ js/charts.js
@@ -1,27 +1,49 @@
-function addOptions(){
-    var request = new XMLHttpRequest();
-        request.open("GET", "./js/data.json", false);
-        request.send(null)
-        var data = JSON.parse(request.responseText);
-        var select = document.getElementById('movieDataset');
-                var option;
-                for (var i = 0; i < data.length; i++) {
-                 option = document.createElement('option');
-                 option.text = data[i]["title_x"];
-                 select.add(option);
-                 
-                }
-           }
-          
-  // Demographics Panel 
-function buildMetadata(title_x) {
+var request = new XMLHttpRequest();
+    request.open("GET", "./js/data.json", false);
+    request.send(null)
+    var data = JSON.parse(request.responseText);
+function init() {
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#movieDataset");
+  
+    // Use the list of sample names to populate the select options
     d3.json("js/data.json").then((data) => {
-      var metadata = data.title_x;
+      var movieNames = data.title_x;
+  
+      movieNames.forEach((movieId) => {
+        selector
+          .append("option")
+          .text(movieId)
+          .property("value", movieId);
+          
+      });
+      
+  
+      // Use the first sample from the list to build the initial plots
+      var firstMovie = movieNames[0];
+      buildCharts(firstMovie);
+      buildMetadata(firstMovie);
+    });
+  }
+  
+  // Initialize the dashboard
+  init();
+  
+  function optionChanged(newMovie) {
+    // Fetch new data each time a new sample is selected
+    buildMetadata(newMovie);
+    buildCharts(newMovie);
+    
+  }
+  // Demographics Panel 
+  function buildMetadata(movieId) {
+    d3.json("js/data.json").then((data) => {
+      var metadata = data.Cluster;
       // Filter the data for the object with the desired sample number
-      var resultArray = metadata.filter(sampleObj => sampleObj.id == title_x);
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == movieId);
       var result = resultArray[0];
       // Use d3 to select the panel with id of `#sample-metadata`
-      var panel = d3.select("#sample-metadata");
+      var panel = d3.select("#movie-metadata");
   
       // Use `.html("") to clear any existing metadata
       panel.html("");
@@ -37,9 +59,7 @@ function buildMetadata(title_x) {
   
     });
   }
-
-
-                
+  
   
     
   
